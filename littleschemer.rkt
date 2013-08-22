b0e733cbbfe6fdca0930c7825d80c5be2ddd61e9
arityfn <arityfn@gmail.com>
2013-08-21 20:06:47 -0500

    added a couple of extra abstractions to lat?

--- littleschemer.rkt
+++ littleschemer.rkt
@@ -1,6 +1,21 @@
 #lang racket
 (require rackunit)
 
+;; ListOfBooleans -> Boolean
+;; takes a listOfBooleans and produces true if all the elements
+;; are true and false otherwise
+(define (my-and xs) (foldr (lambda (a b) (and a b)) true xs))
+
+(check-equal? (my-and (list true true true)) true)
+(check-equal? (my-and (list true true false true)) false)
+
+;; (a -> Boolean) -> ListOfA -> Boolean
+;; takes a predicate and a list, returns true if all elements of the list
+;; satisfy the predicate and false otherwiese.
+(define (all p xs) (my-and (map p xs)))
+
+(check-equal? (all (lambda (x) (< x 11)) (range 0 11)) true)
+
 ;; Atom -> Boolean
 ;; produces true if the argument provided is an atom and false otherwise
 (define (atom? x)
@@ -11,7 +26,6 @@
 (check-eq? (atom? (cons 'b empty)) false)
 (check-eq? (atom? 1) true)
 
-
 ;; ListOfAtom -> Boolean
 ;; produces true if a list consists only of atoms
 (define (lat? loa)
@@ -19,13 +33,24 @@
         [(atom? (car loa)) (lat? (cdr loa))]
         [else false]))
 
+;; with pattern matching
+(define (lat-2? loa)
+  (match loa
+         ['() true]
+         [(cons hd tl) (if (atom? hd)
+                         (lat-2? tl)
+                         false)]))
+
+;; abstacting it with all 
+(define (lat-3? loa)
+  (all (lambda (x) (atom? x)) loa))
+
 (check-eq? (lat? (list 'bacon (list 'and 'eggs))) false)
 (check-eq? (lat? (list 'bacon 'and 'eggs)) true)
-
-(check-eq? (lat? '()) true)
-(check-eq? (lat? (list 1 2 3)) true)
-(check-eq? (lat? (list 'a 'b 'c)) true)
-(check-eq? (lat? (list 1 (list))) false)
+(check-eq? (lat-2? (list 'bacon (list 'and 'eggs))) false)
+(check-eq? (lat-2? (list 'bacon 'and 'eggs)) true)
+(check-eq? (lat-3? (list 'bacon (list 'and 'eggs))) false)
+(check-eq? (lat-3? (list 'bacon 'and 'eggs)) true)
 
 ;; Atom ListOfAtom -> Boolean
 ;; produces true if an atom belongs to the provided list and false otherwise
