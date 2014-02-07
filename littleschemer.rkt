f0dc4bd3a8f02dcec58305edb36dc2fa42993c54
arityfn <arityfn@gmail.com>
2014-02-06 20:25:17 -0600

    finished some stuff

--- littleschemer.rkt
+++ littleschemer.rkt
@@ -25,13 +25,13 @@
 ;; the second argumetns to cons bust be a a list.
 ;; the result is a list.
 
-(check-equal? (cons 1 empty) (list 1))
+(check-equal? (cons 1 '()) (list 1))
 (check-equal? (cons 1 (list 2)) (list 1 2))
 
 ;; The law of Null?
 ;; the primitive null? is defined only for lists.
 
-(check-equal? (null? empty) true)
+(check-equal? (null? '()) true)
 (check-equal? (null? (list 1)) false)
 (check-equal? (null? (list (list ))) false)
 
@@ -44,20 +44,20 @@
                                 
                     ;; 2. Do it, do it again, and again, and again.
 (define (lat? l) 
-  (cond [(empty? l) true]
+  (cond [(null? l) true]
         [(atom? (car l)) (lat? (cdr l))]
         [else false]))
 
-(check-equal? (lat? empty) true)
+(check-equal? (lat? '()) true)
 (check-equal? (lat? (list 1)) true)
 (check-equal? (lat? (list 1 (list 2))) false)
 
 (define (member? a l)
-  (cond [(empty? l) false]
+  (cond [(null? l) false]
         [(eq? a (car l)) true]
         [else (member? a (cdr l))]))
 
-(check-equal? (member? 1 empty) false)
+(check-equal? (member? 1 '()) false)
 (check-equal? (member? 1 (list 1)) true)
 (check-equal? (member? 1 (list 3 2 1)) true)
 
@@ -67,23 +67,23 @@
                         ;; 3. Cons the Magnificient.
 
 (define (rember a lat)
-  (cond [(null? lat) empty]
+  (cond [(null? lat) '()]
         [(eq? a (car lat)) (cdr lat)]
         [else (cons (car lat) (rember a (cdr lat)))]))
 
 
-(check-equal? (rember 1 empty) empty)
-(check-equal? (rember 1 (list 1)) empty)
+(check-equal? (rember 1 '()) '())
+(check-equal? (rember 1 (list 1)) '())
 (check-equal? (rember 1 (list 1 2)) (list 2))
 
 ;; The Second Commandment
 ;; Use cons to buid lists.
 
 (define (firsts l)
-  (cond [(null? l) empty]
+  (cond [(null? l) '()]
         [else (cons (car (car l)) (firsts (cdr l)))]))
 
-(check-equal? (firsts empty) empty)
+(check-equal? (firsts '()) '())
 (check-equal? (firsts (list (list 1))) (list 1))
 (check-equal? (firsts (list (list 1 22) (list 2 3) (list 3 3))) (list 1 2 3))
 
@@ -92,67 +92,67 @@
 ;; and then cons it on the the natural recursion.
 
 (define (insertR n o lat)
-  (cond [(empty? lat) empty]
+  (cond [(null? lat) '()]
         [(eq? o (car lat)) (cons n (cons o ( cdr lat)))]
         [else (insertR (n o (cdr lat)))]))
 
-(check-equal? (insertR 1 2 empty) empty) 
+(check-equal? (insertR 1 2 '()) '()) 
 (check-equal? (insertR 1 2 (list 2 3)) (list 1 2 3))
 (check-equal? (insertR "a" "b" (list "b" "c")) (list "a" "b" "c"))
 
 (define (insertL n o lat)
-  (cond [(empty? lat) empty]
+  (cond [(null? lat) '()]
         [(eq? o (car lat)) (cons o (cons n (cdr lat)))]
         [else (cons (car lat) (insertL n o (cdr lat)))]))
 
-(check-equal? (insertL "c" "b"  empty) empty)
+(check-equal? (insertL "c" "b"  '()) '())
 (check-equal? (insertL "c" "b" (list "a" "b")) (list "a" "b" "c"))
 
 (define (subst n o lat)
-  (cond [(empty? lat) empty]
+  (cond [(null? lat) '()]
         [(eq? o (car lat)) (cons n (cdr lat))]
         [else (cons (car lat) (subst n o (cdr lat)))]))
 
-(check-equal? (subst "new" "old" empty) empty)
+(check-equal? (subst "new" "old" '()) '())
 (check-equal? (subst "new" "old" (list "old" "hello")) (list "new" "hello")) 
 
 (define (subst2 n o1 o2 lat)
-  (cond [(empty? lat) empty]
+  (cond [(null? lat) '()]
         [(or (eq? o1 (car lat))
              (eq? o2 (car lat))) (cons n (cdr lat))]
         [else (cons (car lat) (subst2 n o1 o2 (cdr lat)))]))
 
-(check-equal? (subst2 "new" "old1" "old2" empty) empty)
+(check-equal? (subst2 "new" "old1" "old2" '()) '())
 (check-equal? (subst2 "new" "old1" "old2" (list "old1" "old2")) (list "new" "old2"))
 (check-equal? (subst2 "vanilla" "chocolate" "banana"
                       (list "banana" "ice" "cream" "with" "chocolate" "topping"))
               (list "vanilla" "ice" "cream" "with" "chocolate" "topping"))
 
 (define (multirember a lat)
-  (cond [(empty? lat) empty]
+  (cond [(null? lat) '()]
         [(eq? a (car lat)) (multirember a (cdr lat))]
         [else (cons (car lat) (multirember a (cdr lat)))]))
 
-(check-equal? (multirember "a" empty) empty)
-(check-equal? (multirember "a" (list "a")) empty)
+(check-equal? (multirember "a" '()) '())
+(check-equal? (multirember "a" (list "a")) '())
 (check-equal? (multirember "a" (list "a" "b" "a" "c" "a"))
               (list "b" "c"))
 
 (define (multiinsertR n o lat)
-  (cond [(empty? lat) empty]
+  (cond [(null? lat) '()]
         [(eq? o (car lat)) (cons o (cons n (multiinsertR n o (cdr lat))))]
         [else (cons (car lat) (multiinsertR n o (cdr lat)))]))
 
-(check-equal? (multiinsertR "new" "old" empty) empty)
+(check-equal? (multiinsertR "new" "old" '()) '())
 (check-equal? (multiinsertR  "new" "old" (list "old" "old" "old"))
               (list "old" "new" "old" "new" "old" "new"))
 
 (define (multiinsertL n o lat)
-  (cond [(empty? lat) empty]
+  (cond [(null? lat) '()]
         [(eq? o (car lat)) (cons n (cons o (multiinsertL n o (cdr lat))))]
         [else (cons (car lat) (multiinsertL n o (cdr lat)))]))
 
-(check-equal? (multiinsertL "new" "old" empty) empty)
+(check-equal? (multiinsertL "new" "old" '()) '())
 (check-equal? (multiinsertL  "new" "old" (list "old" "old" "old"))
               (list "new" "old" "new" "old" "new" "old"))
 
@@ -163,11 +163,11 @@
 ;; when using cdr, test terminationwith null?.
 
 (define (multisubst n o lat)
-  (cond [(empty? lat) empty]
+  (cond [(null? lat) '()]
         [(eq? o (car lat)) (cons n (multisubst n o (cdr lat)))]
         [else (cons (car lat) (multisubst n o (cdr lat)))]))
 
-(check-equal? (multisubst "new" "old" empty) empty)
+(check-equal? (multisubst "new" "old" '()) '())
 (check-equal? (multisubst "new" "old" (list "old" 1 "old" 1))
               (list "new" 1 "new" 1))
 
@@ -198,10 +198,10 @@
 ;; about it: (zero? n) and else.
 
 (define (addtup tup)
-  (cond [(empty? tup) 0]
+  (cond [(null? tup) 0]
         [else (my-add (car tup) (addtup (cdr tup)))]))
 
-(check-equal? (addtup empty) 0)
+(check-equal? (addtup '()) 0)
 (check-equal? (addtup (list 1)) 1)
 (check-equal? (addtup (list 1 3)) 4)
 (check-equal? (addtup (list 1 2 3)) 6)
@@ -236,11 +236,11 @@
 ;; of the terminatin line.
 
 (define (tup+ tup1 tup2)
-  (cond [(and (empty? tup1) (empty? tup2)) empty]
+  (cond [(and (null? tup1) (null? tup2)) '()]
         [else (cons (my-add (car tup1) (car tup2))
                     (tup+ (cdr tup1) (cdr tup2)))]))
 
-(check-equal? (tup+ empty empty) empty)
+(check-equal? (tup+ '() '()) '())
 (check-equal? (tup+ (list 3 6 9 11 4)
                     (list 8 5 2 0 7)) (list 11 11 11 11 11))
 
@@ -286,47 +286,47 @@
 (check-equal? (div^ 15 4) 3)
 
 (define (length^ lat)
-  (cond [(empty? lat) 0]
-        [else (add1 (length^ (rest lat)))]))
+  (cond [(null? lat) 0]
+        [else (add1 (length^ (cdr lat)))]))
 
-(check-equal? (length^ empty) 0)
+(check-equal? (length^ '()) 0)
 (check-equal? (length^ (list 'a)) 1)
 (check-equal? (length^ (list 'a 'b)) 2)
 (check-equal? (length^ (list 'a 'b 'c)) 3)
 
 (define (pick n lat)
   (cond [(zero? (sub1 n)) (car lat)]
-        [else (pick (sub1 n) (rest lat))]))
+        [else (pick (sub1 n) (cdr lat))]))
 
 (check-equal? (pick 4 (list 'lasagna 'spaghetti 'ravioli 'macaroni 'meatball)) 'macaroni)
 
 (define (rempick n lat)
   (cond [(zero? (sub1 n)) (cdr lat)]
         [else (cons (car lat)
-                    (rempick (sub1 n) (rest lat)))]))
+                    (rempick (sub1 n) (cdr lat)))]))
 
 (check-equal? (rempick 3 (list 'hotdogs 'with 'hot 'mustard)) (list 'hotdogs 'with 'mustard) '(list 'hotdogs 'with 'mustard))
 
 (define (no-nums lat)
-  (cond [(empty? lat) empty]
-        [(number? (first lat)) (no-nums (rest lat))]
-        [else (cons (car lat) (no-nums (rest lat)))]))
+  (cond [(null? lat) '()]
+        [(number? (car lat)) (no-nums (cdr lat))]
+        [else (cons (car lat) (no-nums (cdr lat)))]))
 
 (check-equal? (no-nums (list 1 3 'x 'b 5 'z 'b))
               (list 'x 'b 'z 'b))
 
 (define (all-nums lat)
-  (cond [(empty? lat) empty]
-        [(number? (first lat)) (cons (car lat) (all-nums (rest lat)))]
-        [else (all-nums (rest lat))]))
+  (cond [(null? lat) '()]
+        [(number? (car lat)) (cons (car lat) (all-nums (cdr lat)))]
+        [else (all-nums (cdr lat))]))
 
 (check-equal? (all-nums (list 1 3 'x 'b 5 'z 'b))
               (list 1 3 5))
 
 (define (occur a lat)
-  (cond [(empty? lat) 0]
-        [(eq? a (first lat)) (add1 (occur a (rest lat)))]
-        [else (occur a (rest lat))]))
+  (cond [(null? lat) 0]
+        [(eq? a (car lat)) (add1 (occur a (cdr lat)))]
+        [else (occur a (cdr lat))]))
 
 (check-equal? (occur 'b (list 'x '2 'x '5 'x)) 0)
 (check-equal? (occur 'x (list 'x '2 'x '5 'x)) 3)
@@ -335,9 +335,9 @@
   (= 1 a))
 
 (define (rempick^ a lat)
-  (cond [(empty? lat) empty]
+  (cond [(null? lat) '()]
         [(one? a) (cdr lat)]
-        [else (cons (first lat)(rempick^ (sub1 a) (rest lat)))]))
+        [else (cons (car lat)(rempick^ (sub1 a) (cdr lat)))]))
 
 (check-equal? (rempick^ 3 (list 'lemon 'meringue 'salty 'pie))
               (list 'lemon 'meringue 'pie))
@@ -345,24 +345,24 @@
                 ;; 5. *Oh My Gawd*: Its Full Of Stars
 
 (define (rember* a l)
-  (cond [(empty? l) empty]
-        [(atom? (first l))
-         (cond [(eq? (first l) a) (rember* a (rest l))]
-               [else (cons (first l) (rember* a (rest l)))])]
-        [else (cons (rember* a (first l))
-                    (rember* a (rest l)))]))
+  (cond [(null? l) '()]
+        [(atom? (car l))
+         (cond [(eq? (car l) a) (rember* a (cdr l))]
+               [else (cons (car l) (rember* a (cdr l)))])]
+        [else (cons (rember* a (car l))
+                    (rember* a (cdr l)))]))
 
-(check-equal? (rember* 'x empty) empty)
+(check-equal? (rember* 'x '()) '())
 (check-equal? (rember* 'x (list 'x (list 'x (list (list 'x)))))
               (list(list(list(list)))))
 
 (define (insertR* new old l)
-  (cond [(empty? l) empty]
-        [(atom? (first l))
-         (cond [(eq? old (first l)) (cons old (cons new (rest l)))]
-               [else (cons (first l) (insertR* new old (rest l)))])]
-        [else (cons (insertR* new old (first l))
-                    (insertR* new old (rest l)))]))
+  (cond [(null? l) '()]
+        [(atom? (car l))
+         (cond [(eq? old (car l)) (cons old (cons new (cdr l)))]
+               [else (cons (car l) (insertR* new old (cdr l)))])]
+        [else (cons (insertR* new old (car l))
+                    (insertR* new old (cdr l)))]))
 
 (check-equal? (insertR* 'y 'x (list 'x 'z)) (list 'x 'y 'z))
 (check-equal? (insertR* 'y 'x (list (list (list 'x 'z))))
@@ -392,14 +392,84 @@
 ;; when using sub1, test termination with zero?
 
 (define (occur* a l)
-  (cond [(empty? l) 0]
-        [(atom? (first l))
-         (cond [(eq? a (first l)) (add1 (occur* a (rest l)))]
-               [else (occur* a (rest l))])]
-        [else (my-add (occur* a (first l))
-                      (occur* a (rest l)))]))
+  (cond [(null? l) 0]
+        [(atom? (car l))
+         (cond [(eq? a (car l)) (add1 (occur* a (cdr l)))]
+               [else (occur* a (cdr l))])]
+        [else (my-add (occur* a (car l))
+                      (occur* a (cdr l)))]))
 
-(check-equal? (occur* 'x empty) 0)
+(check-equal? (occur* 'x '()) 0)
 (check-equal? (occur* 'x (list 'x)) 1)
 (check-equal? (occur* 'x (list (list 'x))) 1)
 (check-equal? (occur* 'x (list (list (list (list 'x))))) 1)
+
+(define (subst* new old l)
+  (cond [(null? l) '()]
+        [(atom? (car l))
+         (cond [(eq? (car l) old) (cons new (subst* new old (cdr l)))]
+               [else (cons (car l) (subst* new old (cdr l)))])]
+        [else (cons (subst* new old (car l))
+                    (subst* new old (cdr l)))]))
+
+(check-equal? (subst* 'x 'b (list (list 'b 'b)))
+              (list (list 'x 'x)))
+
+(define (insertL* new old l)
+  (cond [(null? l) '()]
+        [(atom? (car l))
+         (cond [(eq? old (car l)) (cons new (cons old (cdr l)))]
+               [else (cons (car l) (insertL* (new old (cdr l))))])]
+        [else (cons (insertL* new old (car l))
+                    (insertL* new old (cdr l)))]))
+
+(check-equal? (insertL* 0 1 (list (list 1 2 3)))
+              (list (list 0 1 2 3)))
+
+(define (member* a l)
+  (cond [(null? l) false]
+        [(atom? (car l))
+         (cond [(eq? a (car l)) true]
+               [else (member* a (cdr l))])]
+        [else (or (member* a (car l))
+                  (member* a (cdr l)))]))
+
+(check-equal? (member* 'x (list (list (list 'x)))) true)
+
+(define (leftmost l)
+  (cond [(atom? (car l)) (car l)]
+        [else (leftmost (car l))]))
+
+(check-equal? (leftmost (list (list 'x))) 'x)
+(check-equal? (leftmost (list (list (list (list 'x))))) 'x)
+
+(define (equan? a1 a2)
+  (cond [(and (number? a1) (number? a2)) (= a1 a2)]
+        [(or (number? a1) (number? a2)) false]
+        [else (eq? a1 a2)]))
+
+(check-equal? (equan? 1 1) true)
+(check-equal? (equan? 1 2) false)
+(check-equal? (equan? 'x 'x) true)
+(check-equal? (equan? 'x 'y) false)
+
+(define (eqlist? l1 l2)
+  (cond [(and (null? l1) (null? l2)) true]
+        [(or (null? l1) (null? l2)) false]
+        [(and (atom? (car l1))
+              (atom? (car l2)))
+         (and (equan? (car l1) (car l2))
+              (eqlist? (cdr l1) (cdr l2)))]
+        [(or (atom? (car l1)) (atom? (car l2))) false]
+        [else (and (eqlist? (car l1) (car l2))
+                   (eqlist? (cdr l1) (cdr l2)))]))
+
+(check-equal? (eqlist? empty empty) true)
+(check-equal? (eqlist? empty (list 'x)) false)
+(check-equal? (eqlist? (list 'x) empty) false)
+(check-equal? (eqlist? (list 'x) (list (list 'x))) false)
+(check-equal? (eqlist? (list (list 'x)) (list 'x)) false)
+(check-equal? (eqlist? (list (list 'x))
+                      (list (list 'x))) true)
+(check-equal? (eqlist? (list (list (list 'we 'are 'the 'same)))
+                       (list (list (list 'we 'are 'the 'same)))) true)
